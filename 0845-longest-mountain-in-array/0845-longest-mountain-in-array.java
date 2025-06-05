4c16d64ec0e6a20b2c37dcd643c36d10e2a28828
AnamikaKushwaha <44332984+AnamikaKushwaha@users.noreply.github.com>
2025-06-05 21:36:36 +0530

    Time: 3 ms (64.43%), Space: 44.9 MB (64.64%) - LeetHub

---
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestMountain(int[] arr) {
+        if(arr.length < 3){
+            return 0;
+        }
+        int ans = 0;
+        for(int i = 1; i < arr.length-1; i++){
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                int left = i, right = i;
+            
+                while(left>0 && arr[left-1]<arr[left]){
+                    left--;
+                }
+                while(right<arr.length-1 && arr[right+1]<arr[right]){
+                    right++;
+                }
+                ans = Math.max(ans, right-left+1);
+            }
+            
+            
+        }
+        return ans;
+        
+    }
+}
