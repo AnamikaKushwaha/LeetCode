185bee319f9be4f24569dd05cdfb3bcb4ee5ce55
AnamikaKushwaha <44332984+AnamikaKushwaha@users.noreply.github.com>
2025-06-25 18:10:45 +0530

    Time: 0 ms (100%), Space: 41.9 MB (9.06%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -16,14 +16,17 @@
 class Solution {
     int sum = 0;
     public int sumOfLeftLeaves(TreeNode root) {
+        sum(root);
+        return sum;
+    }
+    void sum(TreeNode root){
         if(root == null){
-            return 0;
+            return ;
         }
         if(root.left != null && root.left.left == null && root.left.right == null){
             sum += root.left.val;
         }
         sumOfLeftLeaves(root.left);
         sumOfLeftLeaves(root.right);
-        return sum;
     }
 }
